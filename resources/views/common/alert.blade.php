f9e2e55590890c628a98140bfc5461955d90773f
giulio <giulio@PC-GIULIO>
2024-12-09 11:38:12 +0100

    fix: dynamic alert handling with Livewire and JavaScript
    - Updated alert structure in `alerts.blade.php` to handle dynamic alerts.
    - Added JavaScript listener for custom `alert` events.
    - Alerts automatically removed after 3 seconds.

--- resources/views/common/alert.blade.php
+++ resources/views/common/alert.blade.php
@@ -1,20 +1,44 @@
-@if(Session::has('message'))
-    <div class="alert alert-info" role="alert">
-        <div class="text-muted">{{Session('message')}}</div>
-    </div>
-@elseif(Session::has('success'))
-    <div class="alert alert-success" role="alert">
-        <div class="text-muted">{{Session('success')}}</div>
-    </div>
-@elseif(Session::has('error'))
-    <div class="alert alert-danger" role="alert">
-        <div class="text-muted">{{Session('error')}}</div>
-    </div>
-@endif
-@if ($errors->any())
-    @foreach ($errors->all() as $error)
-        <div class="alert alert-danger" role="alert">
-            <div class="text-muted">{{$error}}</div>
+<div id="alerts">
+    @if(Session::has('message'))
+        <div class="alert alert-info" role="alert">
+            <div class="text-muted">{{Session('message')}}</div>
         </div>
-    @endforeach
-@endif
+    @elseif(Session::has('success'))
+        <div class="alert alert-success" role="alert">
+            <div class="text-muted">{{Session('success')}}</div>
+        </div>
+    @elseif(Session::has('error'))
+        <div class="alert alert-danger" role="alert">
+            <div class="text-muted">{{Session('error')}}</div>
+        </div>
+    @endif
+    @if ($errors->any())
+        @foreach ($errors->all() as $error)
+            <div class="alert alert-danger" role="alert">
+                <div class="text-muted">{{$error}}</div>
+            </div>
+        @endforeach
+    @endif
+</div>
+
+
+<script>
+    window.addEventListener('alert', event => {
+        const detail = Array.isArray(event.detail) ? event.detail[0] : event.detail;
+
+        const type = detail.type || 'info'; // Tipo di messaggio (info, success, error)
+        const message = detail.message || '';
+
+        const alertContainer = document.getElementById('alerts');
+        const alert = document.createElement('div');
+        alert.className = `alert alert-${type}`;
+        alert.role = 'alert';
+        alert.innerHTML = `<div class="text-muted">${message}</div>`;
+
+        alertContainer.appendChild(alert);
+
+        setTimeout(() => {
+            alert.remove();
+        }, 3000);
+    });
+</script>
